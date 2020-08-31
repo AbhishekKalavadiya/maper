f2414e2bec06df48a9d8a005ae525813231190d1
Abhishek Kalavadiya <51313689+AbhishekKalavadiya@users.noreply.github.com>
2020-08-31 08:59:27 -0700

    heroku webiste deploy part23

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000"
+const API_URL = "http://localhost:5000"
 
 export const listEntries = async() => {
     const response = await fetch (`${API_URL}/api/entry`)
