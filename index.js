eb46e5b10f8cbc3f8d6ec7ac2a8a85283af2bb4c
Abhishek Kalavadiya <51313689+AbhishekKalavadiya@users.noreply.github.com>
2020-08-31 10:07:55 -0700

    heroku webiste deploy part27

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 4626 
 app.listen(port, ()=> {
     console.log(`Listining at http://localhost:${port}`)
 })
