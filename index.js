162531bfd5b3705a33d0b31421aec82be969c4d8
Abhishek Kalavadiya <51313689+AbhishekKalavadiya@users.noreply.github.com>
2020-09-01 04:07:02 -0700

    heroku webiste deploy part40

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 mongoose.connect("mongodb+srv://kalu:abhimk7410@maper.usnux.gcp.mongodb.net/Maper?retryWrites=true&w=majority", {
     useNewUrlParser: true, 
